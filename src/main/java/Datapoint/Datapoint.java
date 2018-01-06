eb1ae51422a55624bf7645e7bdc0a7f83913cb93
Nicolas Mohr <Nico.Mohr@gmx.net>
2018-01-06 18:00:11 +0100

    Getters and setters added to Datapoint

--- src/main/java/Datapoint/Datapoint.java
+++ src/main/java/Datapoint/Datapoint.java
@@ -1,4 +1,38 @@
 package Datapoint;
 
-public class Datapoint {
+import javax.xml.crypto.Data;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Datapoint{
+
+    private double temperature;
+    private double pressure;
+    private double revolutions;
+    
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(double pressure) {
+        this.pressure = pressure;
+    }
+
+    public double getRevolutions() {
+        return revolutions;
+    }
+
+    public void setRevolutions(double revolutions) {
+        this.revolutions = revolutions;
+    }
+
+    public double getTemperature() {
+
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
 }
