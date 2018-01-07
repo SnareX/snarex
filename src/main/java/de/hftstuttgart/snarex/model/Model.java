6e950d1f02ce13e153a502e4cf5352108c64017e
71koel <kogaj.elena@gmail.com>
2018-01-07 15:57:39 +0100

    the methods of CeBarRound have been integrated (Part 1) without comments

--- src/main/java/de/hftstuttgart/snarex/model/Model.java
+++ src/main/java/de/hftstuttgart/snarex/model/Model.java
@@ -1,6 +1,11 @@
 package de.hftstuttgart.snarex.model;
 
+import de.hft.wiinf.cebarround.*;
+
 public class Model {
+	
+	private CeBarRoundDataSensorV2 sensor1 = new CeBarRoundDataSensorV2();
+	private CeBarRoundEvent event1 = null;
 
 	public void insert() {
 
@@ -9,12 +14,22 @@ public class Model {
 	public void read() {
 
 	}
+	
 
 	public void delete() {
 
 	}
 
 	public void connect() {
+		sensor1.startMeasure();
+		sensor1.addListener(new CeBarRoundObserver<SensorEvent>() {
+			
+			@Override
+			public void sensorDataEventListener(SensorEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 
 	}
 
@@ -23,6 +38,7 @@ public class Model {
 	}
 
 	public void close() {
+		sensor1.stopMeasure();
 
 	}
 
