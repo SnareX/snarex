f7234255bd3ed4836459ba54c8720b0ed62fb771
Adrian Nahlik <71naad1bwi@hft-stuttgart.de>
2018-01-09 12:04:14 +0100

    neue, resizable View mit angepasstem Model, Launcher und Controller eingefügt.

--- src/main/java/de/hftstuttgart/snarex/model/Model.java
+++ src/main/java/de/hftstuttgart/snarex/model/Model.java
@@ -1,26 +1,7 @@
 package de.hftstuttgart.snarex.model;
 
-import de.hft.wiinf.cebarround.*;
-import de.hftstuttgart.snarex.sensor.Sensor;
-
-import java.util.Vector;
-
 public class Model {
-
-    /**
-     * Vector containing all Sensor Objects
-     */
-    public Vector<Sensor> sensorVector = new Vector<>();
-    
-	public void addSensor(){
-	    sensorVector.add(new Sensor());
-    }
-
-    public void connectToSensor(int index){
-
-	    sensorVector.elementAt(index).connect();
-    }
-
+	
 	public void insert() {
 
 	}
@@ -28,9 +9,12 @@ public class Model {
 	public void read() {
 
 	}
-	
 
 	public void delete() {
+
+	}
+
+	public void connect() {
 
 	}
 
@@ -38,15 +22,8 @@ public class Model {
 
 	}
 
-	public void closeSensorConnection(int index) {
-		sensorVector.elementAt(index).close();
+	public void close() {
 
-	}
-	public static void main(String[] args){
-		Model model1 = new Model();
-		model1.addSensor();
-		model1.connectToSensor(0);
-		//model1.closeSensorConnection(0);
 	}
 
 }
