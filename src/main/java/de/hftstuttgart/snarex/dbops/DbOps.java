f93455f3a1bc4ae220225992034c844a79f140e5
Nicolas Mohr <Nico.Mohr@gmx.net>
2018-01-14 17:00:39 +0100

    pushtodpqueue method added

--- src/main/java/de/hftstuttgart/snarex/dbops/DbOps.java
+++ src/main/java/de/hftstuttgart/snarex/dbops/DbOps.java
@@ -1,4 +1,28 @@
 package de.hftstuttgart.snarex.dbops;
 
+import de.hftstuttgart.snarex.datapoint.Datapoint;
+import de.hftstuttgart.snarex.model.Model;
+
+import static de.hftstuttgart.snarex.model.Model.dpQueue;
+
 public class DbOps {
+
+    void pushToDpQueue(Datapoint dp){
+
+        //clear the queue in case any unwanted elements remain
+        dpQueue.clear();
+
+        synchronized (dpQueue) {
+
+            //should always be true due to previous clear(),
+            // but remains as a security measure
+            if (!dpQueue.offer(dp)) {
+                dpQueue.offer(dp);
+            }
+
+            //notify consumer
+            dpQueue.notify();
+
+        }
+    }
 }
