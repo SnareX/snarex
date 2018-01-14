2df68a3adaa08f71d88816e7e26bf00bec93c6ba
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2018-01-14 17:21:06 +0100

    created save, delete and getData Method

--- src/main/java/de/hftstuttgart/snarex/dbops/DbOps.java
+++ src/main/java/de/hftstuttgart/snarex/dbops/DbOps.java
@@ -2,6 +2,7 @@ package de.hftstuttgart.snarex.dbops;
 
 
 import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import de.hftstuttgart.snarex.datapoint.Datapoint;
@@ -9,12 +10,15 @@ import de.hftstuttgart.snarex.model.Model;
 
 import static de.hftstuttgart.snarex.model.Model.dpQueue;
 
+import java.util.List;
+
 public class DbOps {
 	
 
 	static SessionFactory factory;
 	
-	public static void getConnection() {
+	/* create Connection to Database */
+	public static void getConnectionToDb() {
 		try {
 			factory = new Configuration().configure().buildSessionFactory();
 			System.out.println("SessionFactory object created successfully. Connection to database is up.");
@@ -22,6 +26,42 @@ public class DbOps {
 			e.printStackTrace();
 		}
 	}
+	
+	/* save Datapoint object into database*/
+	public static void saveData(Datapoint datapoint,String recordName) {
+		try {
+			Session session = factory.openSession();
+			session.beginTransaction();
+			datapoint.setRecordName(recordName);
+			session.save(datapoint);
+			session.getTransaction().commit();
+			System.out.println("Successfully saved");
+			session.close();
+		}catch(HibernateException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/* delete data from table DATAPOINTS with recordName*/
+	public static void deleteData(String recordName) {
+		Session session = factory.openSession();
+		session.beginTransaction();
+		session.createQuery("delete from Datapoint where recordName ="+recordName).executeUpdate();
+		session.getTransaction().commit();
+		System.out.println("Successfully deleted");
+		session.close();
+	}
+	
+	/* get data from database in a list */
+	public static List<Datapoint> getData(String recordName){
+		List <Datapoint> datapoints;
+		Session session = factory.openSession();
+		datapoints = session.createQuery("From Datapoint where recordName ="+recordName).list();
+		session.close();
+		return datapoints;
+	}
+	
+	
     void pushToDpQueue(Datapoint dp){
 
         //clear the queue in case any unwanted elements remain
