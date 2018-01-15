8b773d5a8af357cdb8b6bb944d88e28533f93cc0
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2018-01-15 13:22:02 +0100

    bug fixed

--- src/main/java/de/hftstuttgart/snarex/dbops/DbOpsTest.java
+++ src/main/java/de/hftstuttgart/snarex/dbops/DbOpsTest.java
@@ -8,9 +8,9 @@ import de.hftstuttgart.snarex.model.Model;
 public class DbOpsTest {
 
 	public static void main(String[] args) {
-//		try{
+		try{
 			// connect to database
-			//DbOps.getConnectionDb();
+			DbOps.getConnectionDb();
 			
 			// create new object
 //			Datapoint dtp = new Datapoint(36.2,2.2,8000.1);
@@ -20,19 +20,19 @@ public class DbOpsTest {
 			// DbOps.deleteData("Adrian");
 			
 			//read from database
-//			List<Datapoint> datapoints = DbOps.getData("Adrian");
-//			for(Datapoint dtpt : datapoints) {
-//				System.out.println(dtpt.getId() + " "+dtpt.getDate()+" "+dtpt.getTemperature()+" "+dtpt.getPressure()+" "+dtpt.getRevolutions()+ " "+dtpt.getRecordName());
+		List<Datapoint> datapoints = DbOps.getData("Nicolas");
+		for(Datapoint dtpt : datapoints) {
+			System.out.println(dtpt.getId() + " "+dtpt.getDate()+" "+dtpt.getTemperature()+" "+dtpt.getPressure()+" "+dtpt.getRevolutions()+ " "+dtpt.getRecordName());
+		}
+			
+//			for(Datapoint dtptt : Model.dpList) {
+//				System.out.println(dtptt.getDate()+" "+dtptt.getTemperature()+" "+dtptt.getPressure()+" "+dtptt.getRevolutions());
 //			}
 			
-			for(Datapoint dtptt : Model.dpList) {
-				System.out.println(dtptt.getDate()+" "+dtptt.getTemperature()+" "+dtptt.getPressure()+" "+dtptt.getRevolutions());
-			}
 			
-			
-//		}finally {
-//			DbOps.factory.close();
-//		}
+		}finally {
+			DbOps.factory.close();
+		}
 	}
 
 }
